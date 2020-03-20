ccfc58cb958733ab06dd3c6e6f2d27c39aee1cb9
DAVID HANG <davidnhang@gmail.com>
2020-03-20 11:04:06 -0700

    recursive search thoght words

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -4,7 +4,15 @@ Your function should return a count of how many occurences of ***"th"*** occur w
 Your function must utilize recursion. It cannot contain any loops.
 '''
 def count_th(word):
-    
-    # TBC
-    
-    pass
+    lengthOfWord = len(word)
+    lengthOfSubString = len('th')
+    # base case is when i am looking at an empty string
+    if(lengthOfWord==0 or lengthOfWord<lengthOfSubString):
+        return 0;
+    #now start putting the word together. as we put the word together if we find word by lengthofSubstring-1 = "str2",
+    #we +1 every time we find matching word to keep counter
+    if(word[0:lengthOfSubString] == "th"):
+        return count_th(word[lengthOfSubString-1:])+1;
+    #otherword recursively put the word together.
+    return count_th(word[lengthOfSubString-1:])
+
